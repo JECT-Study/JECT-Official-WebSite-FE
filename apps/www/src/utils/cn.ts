97edb169f645604acabf2bfe869a9a08fb7d6f25
sohyoeun <she020108@gmail.com>
2026-09-14 22:38:45 +0900

    fix: cn()의 JDS 토큰 클래스 병합 오류 수정

--- apps/www/src/utils/cn.ts
+++ apps/www/src/utils/cn.ts
@@ -1,5 +1,9 @@
+import twMergeConfig from "@ject/styles/tw-merge.json" with { type: "json" };
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+// tailwind-merge가 JDS 토큰 클래스를 올바르게 병합하도록 토큰 이름을 알려준다. 설정은 `pnpm gen:theme`으로 생성한다.
+const twMerge = extendTailwindMerge(twMergeConfig);
 
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
